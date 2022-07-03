93c6604cf2a9571d3c5808834312257853e78054
gongbo <bo.gong@accenture.com>
2022-07-03 23:01:47 +0800

    [gongbo] #N/A feat: init project

---
+++ src/main/java/com/bo/shardingspheredemo/ShardingsphereDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bo.shardingspheredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingsphereDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingsphereDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bo/shardingspheredemo/ShardingsphereDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo.shardingspheredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingsphereDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
